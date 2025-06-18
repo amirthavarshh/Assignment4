3d89dcb4de6aceb20be22d3064cf1f839e095403
amirthavarshh <amirthagopinath2@gmail.com>
2025-06-18 16:02:12 +0530

    Add files via upload

---
+++ a4 task1.py
@@ -0,0 +1,7 @@
+try:
+    print("Reading file content:")
+    with open("sample.txt", "r") as file:
+        for i, line in enumerate(file, start=1):
+            print(f"Line {i}: {line.strip()}")
+except FileNotFoundError:
+    print("Error: The file 'sample.txt' was not found.")
